6ea04b0d704bcfb154d8c71c1965dea2123f5f1d
Agung Firmansyah <agungfirdev@gmail.com>
2024-09-05 09:11:06 +0700

    add lazy loading, info

--- src/show_pbp.js
+++ src/show_pbp.js
@@ -84,87 +84,100 @@
 // });
 
 function showPbp(filter) {
-  let jumlahPengganti = 0;
-  let jumlahPerwakilan = 0;
-  let jumlahAwal = 0;
-  filter.forEach(
-    (
-      {
-        NO,
-        NO_KPM,
-        NIK_PBP_AWAL,
-        NIK_PBP_PENERIMA,
-        NAMA_PBP_AWAL,
-        NAMA_PENERIMA,
-        STATUS,
-        PENERIMA,
-        KOTA,
-        KECAMATAN,
-        DESA,
-        FOTO_URL,
-      },
-      index
-    ) => {
-      if (STATUS === "PENGGANTI") {
-        jumlahPengganti++;
+  if (filter.length !== 0) {
+    let jumlahPengganti = 0;
+    let jumlahPerwakilan = 0;
+    let jumlahSisa = 0;
+    let jumlahAwal = 0;
+    filter.forEach(
+      (
+        {
+          NO,
+          NO_KPM,
+          NIK_PBP_AWAL,
+          NIK_PBP_PENERIMA,
+          NAMA_PBP_AWAL,
+          NAMA_PENERIMA,
+          STATUS,
+          PENERIMA,
+          KOTA,
+          KECAMATAN,
+          DESA,
+          FOTO_URL,
+        },
+        index
+      ) => {
+        if (STATUS === "PENGGANTI") {
+          jumlahPengganti++;
+        }
+
+        if (STATUS === "PERWAKILAN") {
+          jumlahPerwakilan++;
+        }
+
+        if (STATUS === "AWAL") {
+          jumlahAwal++;
+        }
+
+        if (STATUS === "") {
+          jumlahSisa++;
+        }
+
+        const url = `https://astridjplb.id/files/img/dokumen/47/JAWATENGAH/${KOTA.replace(
+          " ",
+          ""
+        ).replace(".", "")}/${KECAMATAN.replace(" ", "")}/${DESA.replace(
+          " ",
+          ""
+        )}/${FOTO_URL.split(".")[0]}.jpg`;
+
+        const urlKtp = `https://astridjplb.id/files/img/dokumen/47/JAWATENGAH/${KOTA.replace(
+          " ",
+          ""
+        ).replace(".", "")}/${KECAMATAN.replace(" ", "")}/${DESA.replace(
+          " ",
+          ""
+        )}/${FOTO_URL.split(".")[0]}_ktp.jpg`;
+
+        let TEXT_STATUS = "";
+
+        if (STATUS === "AWAL") {
+          TEXT_STATUS = `<span class="badge rounded-pill bg-dark">AWAL</span>`;
+        } else if (STATUS === "PENGGANTI") {
+          TEXT_STATUS = `<span class="badge rounded-pill bg-success">PENGGANTI</span>`;
+        } else if (STATUS === "PERWAKILAN") {
+          TEXT_STATUS = `<span class="badge rounded-pill bg-primary">PERWAKILAN</span>`;
+        } else {
+          TEXT_STATUS = `<span class="badge rounded-pill bg-danger">BELUM UP</span>`;
+        }
+        $("#table-body").append(`
+                    <tr role="button" data-bs-toggle="collapse" data-bs-target="#${NIK_PBP_AWAL}" aria-expanded="false" aria-controls="${NIK_PBP_AWAL}">
+                        <th scope="row">${NO}</th>
+                        <td>${NO_KPM}</td>
+                        <td>${NIK_PBP_AWAL}</td>
+                        <td>${NIK_PBP_PENERIMA}</td>
+                        <td>${NAMA_PBP_AWAL}</td>
+                        <td>${NAMA_PENERIMA}</td>
+                        <td>${TEXT_STATUS}</td>
+                    </tr>
+                    <tr id="${NIK_PBP_AWAL}" class="collapse">
+                        <td colspan="4">
+                            <div class="d-flex">
+                                <img loading="lazy" src="${url}" width="100%" id="I${NIK_PBP_AWAL}" class="img-zoom-pbp"/>
+                                <img loading="lazy" src="${urlKtp}" width="100%" id="K${NIK_PBP_AWAL}" class="img-zoom-ktp"/>
+                            </div>
+                        </td>
+                   
+                    </tr>
+                `);
+        $("#jumlah-pbp").html("JUMLAH |  " + filter.length);
+        $("#jumlah-awal").html("AWAL |  " + jumlahAwal);
+        $("#jumlah-pengganti").html("PENGGANTI |  " + jumlahPengganti);
+        $("#jumlah-perwakilan").html("PERWAKILAN |  " + jumlahPerwakilan);
+        $("#jumlah-sisa").html("SISA |  " + jumlahSisa);
       }
-
-      if (STATUS === "PERWAKILAN") {
-        jumlahPerwakilan++;
-      }
-
-      if (STATUS === "AWAL") {
-        jumlahAwal++;
-      }
-
-      const url = `https://astridjplb.id/files/img/dokumen/47/JAWATENGAH/${KOTA.replace(
-        " ",
-        ""
-      ).replace(".", "")}/${KECAMATAN.replace(" ", "")}/${DESA.replace(
-        " ",
-        ""
-      )}/${FOTO_URL.split(".")[0]}.jpg`;
-
-      const urlKtp = `https://astridjplb.id/files/img/dokumen/47/JAWATENGAH/${KOTA.replace(
-        " ",
-        ""
-      ).replace(".", "")}/${KECAMATAN.replace(" ", "")}/${DESA.replace(
-        " ",
-        ""
-      )}/${FOTO_URL.split(".")[0]}_ktp.jpg`;
-
-      let TEXT_STATUS = "";
-
-      if (STATUS === "AWAL") {
-        TEXT_STATUS = `<button class="badge rounded-pill bg-dark">AWAL</span>`;
-      } else if (STATUS === "PENGGANTI") {
-        TEXT_STATUS = `<button class="badge rounded-pill bg-success">PENGGANTI</span>`;
-      } else {
-        TEXT_STATUS = `<button class="badge rounded-pill bg-primary">PERWAKILAN</span>`;
-      }
-      $("#table-body").append(`
-                <tr role="button" data-bs-toggle="collapse" data-bs-target="#${NIK_PBP_AWAL}" aria-expanded="false" aria-controls="${NIK_PBP_AWAL}">
-                    <th scope="row">${NO}</th>
-                    <td>${NO_KPM}</td>
-                    <td>${NIK_PBP_AWAL}</td>
-                    <td>${NIK_PBP_PENERIMA}</td>
-                    <td>${NAMA_PBP_AWAL}</td>
-                    <td>${NAMA_PENERIMA}</td>
-                    <td>${TEXT_STATUS}</td>
-                </tr>
-                <tr id="${NIK_PBP_AWAL}" class="collapse">
-                    <td colspan="3">
-                        <div class="d-flex">
-                            <img src="${url}" width="100%"/>
-                            <img src="${urlKtp}" width="100%"/>
-                        </div>
-                    </td>
-                </tr>
-            `);
-      $("#jumlah-pbp").html("JUMLAH |  " + filter.length);
-      $("#jumlah-awal").html("AWAL |  " + jumlahAwal);
-      $("#jumlah-pengganti").html("PENGGANTI |  " + jumlahPengganti);
-      $("#jumlah-perwakilan").html("PERWAKILAN |  " + jumlahPerwakilan);
-    }
-  );
+    );
+  } else {
+    $("#table-body").html("");
+  }
 }
