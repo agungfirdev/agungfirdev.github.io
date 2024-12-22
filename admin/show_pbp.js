d4fbcd5dd626428dee79d02210dfa4f4d61ed4f4
Agung Firmansyah <agungfirdev@gmail.com>
2024-12-22 21:21:07 +0700

    update

--- admin/show_pbp.js
+++ admin/show_pbp.js
@@ -155,7 +155,7 @@ function showPbp(PBPS) {
                                 isShow ? "show" : ""
                               }">
                             <!--- <td class="bg-transparent"></td> --->
-                            <td colspan="7">
+                            <td colspan="4">
                                 <div class="d-flex" width="100%">
                                     <img loading="lazy" src="${url}" width="100%" id="I${NIK}" class="object-fit-contain"/>
                                     <img loading="lazy" src="${urlKtp}" width="100%" id="K${NIK}" class="zoom-move object-fit-contain" onclick="rotateImage('#K${NIK}')"/>
